7fad7a99c6fd5ebaeb6e445b9f1d81e6c4fac495
Петър Георгиев <peturgeorgievv@gmail.com>
2019-11-12 13:16:38 +0200

    Added FireBase to project

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDtx7KhpZ3zk_TDIlka4b3Efl9hXHLWI7w',
+    authDomain: 'tick42p2p.firebaseapp.com',
+    databaseURL: 'https://tick42p2p.firebaseio.com',
+    projectId: 'tick42p2p',
+    storageBucket: 'tick42p2p.appspot.com',
+    messagingSenderId: '744857795647',
+    appId: '1:744857795647:web:05dfff403d4183c6e80c8a',
+    measurementId: 'G-KNZXLHWS4E'
+  }
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDtx7KhpZ3zk_TDIlka4b3Efl9hXHLWI7w',
+    authDomain: 'tick42p2p.firebaseapp.com',
+    databaseURL: 'https://tick42p2p.firebaseio.com',
+    projectId: 'tick42p2p',
+    storageBucket: 'tick42p2p.appspot.com',
+    messagingSenderId: '744857795647',
+    appId: '1:744857795647:web:05dfff403d4183c6e80c8a',
+    measurementId: 'G-KNZXLHWS4E'
+  }
 };
 
 /*
