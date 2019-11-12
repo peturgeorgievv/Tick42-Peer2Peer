610c9a917d5b5df5989d2603116de1a821429667
Петър Георгиев <peturgeorgievv@gmail.com>
2019-11-12 15:20:53 +0200

    Adding boilerplate features

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { FirebaseService } from "./services/firebase.service";
+import { environment } from "./../environments/environment";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AngularFireModule } from "@angular/fire";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { BorrowerComponent } from './features/borrower/borrower.component';
+import { InvestorComponent } from './features/investor/investor.component';
+import { UsersComponent } from './features/users/users.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, DashboardComponent, BorrowerComponent, InvestorComponent, UsersComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule
   ],
-  providers: [],
+  providers: [FirebaseService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ client/src/app/services/firebase.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirebaseService {
+
+  constructor(private readonly db: AngularFirestore) { }
+}
