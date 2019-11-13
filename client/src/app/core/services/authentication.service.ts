41247cdc7a2dd79064a05509010a8bf8904b83c0
Петър Георгиев <peturgeorgievv@gmail.com>
2019-11-13 19:05:50 +0200

    Changes to auth service

--- client/src/app/core/services/authentication.service.ts
+++ client/src/app/core/services/authentication.service.ts
@@ -1,43 +1,75 @@
-import { Injectable } from "@angular/core";
-import { AngularFireAuth } from "@angular/fire/auth";
-import { Observable } from "rxjs";
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Observable, of } from 'rxjs';
+import { User } from 'firebase';
+import { Router } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
 
 @Injectable({
-  providedIn: "root"
+	providedIn: 'root'
 })
 export class AuthenticationService {
-  userData: Observable<firebase.User>;
+	user: Observable<User>;
 
-  constructor(private angularFireAuth: AngularFireAuth) {
-    this.userData = angularFireAuth.authState;
-  }
+	constructor(
+		private angularFireAuth: AngularFireAuth,
+		private angularFireStore: AngularFirestore,
+		private readonly router: Router
+	) {
+		this.user = this.angularFireAuth.authState.pipe(
+			switchMap((user) => {
+				if (user) {
+					return this.angularFireStore.doc<User>(`users/${user.uid}`).valueChanges();
+				} else {
+					return of(null);
+				}
+			})
+		);
+	}
 
-  /* Sign up */
-  SignUp(email: string, password: string) {
-    this.angularFireAuth.auth
-      .createUserWithEmailAndPassword(email, password)
-      .then(res => {
-        console.log("Successfully signed up!", res);
-      })
-      .catch(error => {
-        console.log("Something is wrong:", error.message);
-      });
-  }
+	private updateUserData(user) {
+		// Sets user data to firestore on login
 
-  /* Sign in */
-  SignIn(email: string, password: string) {
-    this.angularFireAuth.auth
-      .signInWithEmailAndPassword(email, password)
-      .then(res => {
-        console.log("Successfully signed in!");
-      })
-      .catch(err => {
-        console.log("Something is wrong:", err.message);
-      });
-  }
+		const userRef: AngularFirestoreDocument<any> = this.angularFireStore.doc(`users/${user.uid}`);
 
-  /* Sign out */
-  SignOut() {
-    this.angularFireAuth.auth.signOut();
-  }
+		const data = {
+			uid: user.uid,
+			email: user.email,
+			displayName: user.displayName
+		};
+
+		return userRef.set(data, { merge: true });
+	}
+
+	/* Sign up */
+	SignUp(email: string, password: string) {
+		this.angularFireAuth.auth
+			.createUserWithEmailAndPassword(email, password)
+			.then((res) => {
+				return res;
+			})
+			.catch((error) => {
+				console.log('Something is wrong:', error.message);
+			});
+	}
+
+	/* Sign in */
+	SignIn(email: string, password: string) {
+		this.angularFireAuth.auth
+			.signInWithEmailAndPassword(email, password)
+			.then((res) => {
+				this.updateUserData(res);
+				this.router.navigate([ '/dashboard' ]);
+				console.log('Successfully signed in!');
+			})
+			.catch((err) => {
+				console.log('Something is wrong:', err.message);
+			});
+	}
+
+	/* Sign out */
+	SignOut() {
+		this.angularFireAuth.auth.signOut();
+	}
 }
--- client/src/app/shared/components/navbar/navbar.component.ts
+++ client/src/app/shared/components/navbar/navbar.component.ts
@@ -1,15 +1,13 @@
+import { AuthenticationService } from './../../../core/services/authentication.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+	selector: 'app-navbar',
+	templateUrl: './navbar.component.html',
+	styleUrls: [ './navbar.component.css' ]
 })
 export class NavbarComponent implements OnInit {
+	constructor(public authService: AuthenticationService) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+	ngOnInit() {}
 }
