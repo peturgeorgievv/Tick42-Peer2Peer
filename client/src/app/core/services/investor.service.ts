24513e83e5cadc24207e63a42a0c8755d5e7dc6d
Teodor Tsvetanov <teodor.tzvetanov@gmail.com>
2019-12-03 20:05:40 +0200

    Current Investments calcs

--- client/src/app/core/services/investor.service.ts
+++ client/src/app/core/services/investor.service.ts
@@ -4,37 +4,45 @@ import { AngularFirestore } from '@angular/fire/firestore';
 import { ThrowStmt } from '@angular/compiler';
 
 @Injectable({
-	providedIn: 'root'
+  providedIn: 'root'
 })
 export class InvestorService {
-	constructor(private angularFireStore: AngularFirestore) {}
+  constructor(private angularFireStore: AngularFirestore) { }
 
-	public getAllLoanRequests() {
-		return this.angularFireStore
-			.collection('requests', (ref) => ref.where('status', '==', StatusENUM.requestOpen))
-			.valueChanges();
-	}
+  public getAllLoanRequests() {
+    return this.angularFireStore
+      .collection('requests', (ref) => ref.where('status', '==', StatusENUM.requestOpen))
+      .valueChanges();
+  }
 
-	public createLoanSuggestion(loanData) {
-		return this.angularFireStore.collection('suggestions').add(loanData);
-	}
+  public createLoanSuggestion(loanData) {
+    return this.angularFireStore.collection('suggestions').add(loanData);
+  }
 
-	public addSuggestionId(refId) {
-		return this.angularFireStore
-			.collection('suggestions')
-			.doc(refId)
-			.set({ $suggestionId: refId }, { merge: true });
-	}
+  public addSuggestionId(refId) {
+    return this.angularFireStore
+      .collection('suggestions')
+      .doc(refId)
+      .set({ $suggestionId: refId }, { merge: true });
+  }
 
-	public getUserInvestments(userId: string) {
-		return this.angularFireStore
+  public getUserInvestments(userId: string) {
+    return this.angularFireStore
       .collection('loans', (ref) => ref
-      .where('$investorId', '==', userId)
-      .where('status', '==', StatusENUM.current))
-			.valueChanges();
-	}
+        .where('$investorId', '==', userId)
+        .where('status', '==', StatusENUM.current))
+      .valueChanges();
+  }
 
-	public getUserDocData(userId: string) {
-		return this.angularFireStore.collection('users').doc(userId).get();
-	}
+  public getUserDocData(userId: string) {
+    return this.angularFireStore.collection('users').doc(userId).get();
+  }
+
+  public getPayments(suggestionId: string, userId: string) {
+    return this.angularFireStore
+      .collection('paymentsHistory', (ref) =>
+        ref.where('$suggestionId', '==', suggestionId).where('$userId', '==', userId)
+      )
+      .valueChanges();
+  }
 }
