a6d62f23674636acd44309b60bf32919b05fb216
Петър Георгиев <peturgeorgievv@gmail.com>
2019-12-07 19:02:54 +0200

    Added form for registering an Admin user

--- client/src/app/features/users/users.module.ts
+++ client/src/app/features/users/users.module.ts
@@ -1,13 +1,17 @@
+import { NgbActiveModal, NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 import { SharedModule } from './../../shared/shared.module';
 import { UsersComponent } from './users.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsersRouterModule } from './users-routing.module';
 import { ShowDetailsModalComponent } from './show-details-modal/show-details-modal.component';
+import { CreateAdminModalComponent } from './create-admin-modal/create-admin-modal.component';
 
 @NgModule({
-	declarations: [ UsersComponent, ShowDetailsModalComponent ],
-	imports: [ CommonModule, SharedModule, UsersRouterModule ],
-	exports: [ UsersComponent ]
+	declarations: [ UsersComponent, ShowDetailsModalComponent, CreateAdminModalComponent ],
+	imports: [ CommonModule, SharedModule, UsersRouterModule, NgbModalModule ],
+	exports: [ UsersComponent ],
+	providers: [ NgbActiveModal ],
+	entryComponents: [ CreateAdminModalComponent, ShowDetailsModalComponent ]
 })
 export class UsersModule {}
