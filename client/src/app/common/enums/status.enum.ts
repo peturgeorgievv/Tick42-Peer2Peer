29db43067746ef80bc9095cb65df6ec8f94008ae
Петър Георгиев <peturgeorgievv@gmail.com>
2019-12-11 20:29:41 +0200

    Refactored const to Enum for status

--- client/src/app/common/enums/status.enum.ts
+++ client/src/app/common/enums/status.enum.ts
@@ -1,9 +1,9 @@
-export const StatusENUM = {
-	current: 'current',
-	paid: 'paid',
-	requestOpen: 'request-open',
-	requestClosed: 'request-closed',
-	suggestionPending: 'suggestion-pending',
-	suggestionAccepted: 'suggestion-accepted',
-	suggestionRejected: 'suggestion-rejected'
-};
+export enum StatusENUM {
+  current = 'current',
+  paid = 'paid',
+  requestOpen = 'request-open',
+  requestClosed = 'request-closed',
+  suggestionPending = 'suggestion-pending',
+  suggestionAccepted = 'suggestion-accepted',
+  suggestionRejected = 'suggestion-rejected'
+}
