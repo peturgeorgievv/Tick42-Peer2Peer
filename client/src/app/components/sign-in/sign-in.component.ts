5edd9611bbf4e5ff869191a4bf602ba9955ed08c
Петър Георгиев <peturgeorgievv@gmail.com>
2019-12-14 13:33:16 +0200

    Added validation to sign-in forms

--- client/src/app/components/sign-in/sign-in.component.ts
+++ client/src/app/components/sign-in/sign-in.component.ts
@@ -1,28 +1,31 @@
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../../core/services/authentication.service';
 import { SignInDTO } from 'src/app/common/models/users/sign-in.dto';
 
 @Component({
-	selector: 'app-sign-in',
-	templateUrl: './sign-in.component.html',
-	styleUrls: [ './sign-in.component.css' ]
+  selector: 'app-sign-in',
+  templateUrl: './sign-in.component.html',
+  styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent implements OnInit {
-	public signinForm: FormGroup;
-	public data: SignInDTO;
+  public signinForm: FormGroup;
+  public data: SignInDTO;
 
-	constructor(public authenticationService: AuthenticationService, public formBuilder: FormBuilder) {}
+  constructor(
+    public authenticationService: AuthenticationService,
+    public formBuilder: FormBuilder
+  ) {}
 
-	ngOnInit() {
-		this.signinForm = this.formBuilder.group({
-			email: [ '' ],
-			password: [ '' ]
-		});
-	}
+  ngOnInit() {
+    this.signinForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
 
-	signIn() {
-		this.data = this.signinForm.value;
-		this.authenticationService.signIn(this.data.email, this.data.password);
-	}
+  signIn() {
+    this.data = this.signinForm.value;
+    this.authenticationService.signIn(this.data.email, this.data.password);
+  }
 }
